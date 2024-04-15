da9cf82c3fe62be08b44b65e704d92e3eab61dae
Ignasiuzz <ignolaiskai@gmail.com>
2024-04-15 14:32:18 +0300

    Class test file
    Pridėtas klasės veikimo testavimo failas

---
+++ test.cpp
@@ -0,0 +1,81 @@
+#include "VectorIncludes.h"
+#include "VectorHeader.h"
+#include "Studentas.h"
+#include <cassert>
+//g++ -o program Vector.cpp Studentas.cpp
+
+void test_constructors() {
+    // Test default constructor
+    Studentas s1;
+    cout << "Test default constructor " << s1.Vardas() << endl;
+    cout << "Test default constructor " << s1.Pavarde() << endl;
+    cout << "Test default constructor ";
+    for (double grade : s1.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test default constructor " << s1.Egzaminas() << endl;
+
+    // Test constructor with input stream
+    istringstream iss("John Doe 1 1 1 1 1");
+    Studentas s2;
+    s2.readStudent(iss);
+    cout << "Test constructor with input stream " << s2.Vardas() << endl;
+    cout << "Test constructor with input stream " << s2.Pavarde() << endl;
+    cout << "Test constructor with input stream ";
+    for (double grade : s2.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test constructor with input stream " << s2.Egzaminas() << endl;
+
+    // Test copy constructor
+    Studentas s3 = s2;
+    cout << "Test copy constructor " << s3.Vardas() << endl;
+    cout << "Test copy constructor " << s3.Pavarde() << endl;
+    cout << "Test copy constructor ";
+    for (double grade : s3.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test copy constructor " << s3.Egzaminas() << endl;
+
+    // Test move constructor
+    Studentas s4 = move(s3);
+    cout << "Test move constructor " << s4.Vardas() << endl;
+    cout << "Test move constructor " << s4.Pavarde() << endl;
+    cout << "Test move constructor ";
+    for (double grade : s4.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test move constructor " << s4.Egzaminas() << endl;
+}
+
+void test_assignment_operators() {
+    // Test copy assignment
+    Studentas s1("John", "Doe", 2, {2, 2, 2, 2});
+    Studentas s2;
+    s2 = s1;
+    cout << "Test copy assignment " << s2.Vardas() << endl;
+    cout << "Test copy assignment " << s2.Pavarde() << endl;
+    cout << "Test copy assignment ";
+    for (double grade : s2.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test copy assignment " << s2.Egzaminas() << endl;
+
+    // Test move assignment
+    Studentas s3;
+    s3 = move(s2);
+    cout << "Test move assignment " << s3.Vardas() << endl;
+    cout << "Test move assignment " << s3.Pavarde() << endl;
+    cout << "Test move assignment ";
+    for (double grade : s3.Namudarbas()) {
+        std::cout << grade << " ";
+    }
+    cout << endl << "Test move assignment " << s3.Egzaminas() << endl;
+}
+
+int main() {
+    test_constructors();
+    test_assignment_operators();
+
+    system ("pause");
+}
