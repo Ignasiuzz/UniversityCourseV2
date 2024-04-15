5d6f84837d4fa43a5abb340b24a329d1990244f9
Ignasiuzz <ignolaiskai@gmail.com>
2024-04-15 14:30:31 +0300

    Rule of Five header
    Pridėtas rule of five headerio pataisymas

--- Studentas.h
+++ Studentas.h
@@ -15,12 +15,21 @@ private:
     static double Mediana(const std::vector<double>& namudarbas);
 public:
 // interfeisas
-    Studentas() : egzaminas_(0) { }  // default konstruktorius
+    Studentas() : egzaminas_(0), namudarbas_({}) {}  // default konstruktorius
     Studentas(const std::string& vardas, const std::string& pavarde, double egzaminas, const std::vector<double>& namudarbas) : Vardas_(vardas), Pavarde_(pavarde), egzaminas_(egzaminas), namudarbas_(namudarbas) {}
     Studentas(std::istream& is);
 
+    // Rule of Five
+    ~Studentas();
+    Studentas(const Studentas& other); // Copy constructor
+    Studentas(Studentas&& other) noexcept; // Move constructor
+    Studentas& operator=(const Studentas& other); // Copy assignment operator
+    Studentas& operator=(Studentas&& other) noexcept; // Move assignment operator
+    
     std::string Vardas() const { return Vardas_; }
     std::string Pavarde() const { return Pavarde_; }
+    double Egzaminas() const { return egzaminas_; }
+    std::vector<double> Namudarbas() const { return namudarbas_; }
     void setVardas(const std::string& vardas);
     void setPavarde(const std::string& pavarde);
     void setEgzaminas(double egzaminas);
@@ -29,7 +38,5 @@ public:
 
     friend double GalutinisVid(const Studentas& duom);
     friend double GalutinisMed(const Studentas& duom);
-
-    ~Studentas();
 };
 #endif
