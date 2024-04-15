5dc9e37f840116b15946f852659c5378946ea9da
Ignasiuzz <ignolaiskai@gmail.com>
2024-04-15 18:42:01 +0300

    Abstrakčios klasės pridėjimas
    Sena klasė pritaikyta veikimui su abstrakčia klase

--- Studentas.h
+++ Studentas.h
@@ -1,42 +1,51 @@
 #ifndef STUDENTAS_H
 #define STUDENTAS_H
 
-#include "VectorIncludes.h"
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Zmogus.h"
 
-class Studentas {
+class Studentas : public Zmogus{
 private:
-// realizacija
-    std::string Vardas_;
-    std::string Pavarde_;
     double egzaminas_;
     std::vector<double> namudarbas_;
 
-    static double Vidurkis(const std::vector<double>& namudarbas);
-    static double Mediana(const std::vector<double>& namudarbas);
 public:
-// interfeisas
-    Studentas() : egzaminas_(0), namudarbas_({}) {}  // default konstruktorius
-    Studentas(const std::string& vardas, const std::string& pavarde, double egzaminas, const std::vector<double>& namudarbas) : Vardas_(vardas), Pavarde_(pavarde), egzaminas_(egzaminas), namudarbas_(namudarbas) {}
-    Studentas(std::istream& is);
+    // Default konstruktoriai
+    Studentas() : Zmogus(), egzaminas_(0), namudarbas_({}) {}
+    Studentas(const std::string& vardas, const std::string& pavarde, double egzaminas, const std::vector<double>& namudarbas) : Zmogus(vardas, pavarde), egzaminas_(egzaminas), namudarbas_(namudarbas) {}
 
     // Rule of Five
     ~Studentas();
     Studentas(const Studentas& other); // Copy constructor
     Studentas(Studentas&& other) noexcept; // Move constructor
-    Studentas& operator=(const Studentas& other); // Copy assignment operator
-    Studentas& operator=(Studentas&& other) noexcept; // Move assignment operator
+    Studentas& operator=(const Studentas& other); // Copy assignment
+    Studentas& operator=(Studentas&& other) noexcept; // Move assignment
     
+    // Outputeriai
     std::string Vardas() const { return Vardas_; }
     std::string Pavarde() const { return Pavarde_; }
     double Egzaminas() const { return egzaminas_; }
     std::vector<double> Namudarbas() const { return namudarbas_; }
+
+    // Set'eriai
     void setVardas(const std::string& vardas);
     void setPavarde(const std::string& pavarde);
     void setEgzaminas(double egzaminas);
     void setNamudarbas(const std::vector<double>& namudarbas);
-    std::istream& readStudent(std::istream&);  // set'eriai
+    std::istream& readStudent(std::istream&);
 
+    // Calculations
     friend double GalutinisVid(const Studentas& duom);
     friend double GalutinisMed(const Studentas& duom);
+    
+    // Static functions for calculations
+    static double Vidurkis(const std::vector<double>& namudarbas);
+    static double Mediana(const std::vector<double>& namudarbas);
+
+    // Virtualios funkcijos deklaracija, kad klase Zmogus butu abstrakti
+    virtual void doSomething();
 };
 #endif
