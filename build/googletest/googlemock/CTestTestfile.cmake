b3696e18f9fe1a838568997a0521b81811b10e96
Ignasiuzz <ignolaiskai@gmail.com>
2024-05-14 14:17:10 +0300

    Cleanup
    Cleanup

--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV2/googletest/googlemock
-# Build directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV2/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV2/googletest/googletest
-# Build directory: C:/Users/ignol/Documents/GitHub/UniversityCourseV2/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
